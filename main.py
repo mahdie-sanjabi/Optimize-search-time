cf60a50a49d7d01987511dd338e190ecf89ff2d2
mahdie <sanjabimahdie44@gmail.com>
2024-12-03 15:35:16 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+def print_hi(name):
+
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def pp():
+    pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
