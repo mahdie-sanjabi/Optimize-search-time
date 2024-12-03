9da19544ef3b1cf002c51c40463ab2bbeb4bfb91
mahdie <sanjabimahdie44@gmail.com>
2024-12-03 15:46:18 +0330

    Add text file and inline search code

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
+import time
+
+def linear_search(database, target):
+    """
+    Perform a linear search to find a username in the database.
+    :param database: List of usernames
+    :param target: The username to find
+    :return: Index of the username if found, otherwise -1
+    """
+    for index, username in enumerate(database):
+        if username == target:
+            return index  # Username found
+    return -1  # Username not found
 
 
-def print_hi(name):
+def read_database_from_file(file_path):
+    """
+    Read usernames from a file and return them as a list.
+    :param file_path: Path to the file containing usernames
+    :return: List of usernames
+    """
+    with open(file_path, 'r') as file:
+        return [line.strip() for line in file]
 
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+# Example Usage
+file_path = "usernames.txt"  # Path to the file containing usernames
+target_username = "zl"   # Username to search for
 
-def pp():
-    pass
+# Read the database from the file
+database = read_database_from_file(file_path)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Measure execution time
+start_time = time.time()
+result = linear_search(database, target_username)
+end_time = time.time()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Output the result
+if result != -1:
+    print(f"Username '{target_username}' found at index {result}.")
+else:
+    print(f"Username '{target_username}' not found.")
+
+# Print execution time
+print(f"Execution time: {end_time - start_time:.6f} seconds")
