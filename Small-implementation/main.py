5c051d5b3167db1455b23ffdff70a2569836f984
mahdie <sanjabimahdie44@gmail.com>
2024-12-04 10:37:56 +0330

    Add class Node, BPluseTree

--- Small-implementation/main.py
+++ Small-implementation/main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
+class Node:
+    """A node in the B+ Tree"""
+    def __init__(self, is_leaf=True):
+        self.is_leaf = is_leaf
+        self.keys = []        # Stores the keys in the node
+        self.children = []    # Stores the children (if non-leaf node)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class BPlusTree:
+    """B+ Tree for storing and searching usernames"""
+    def __init__(self, t=4):  # t is the minimum degree of the tree
+        self.root = Node(is_leaf=True)
+        self.t = t
 
+    def search(self, key, node=None):
+        """Search for a key in the tree"""
+        if node is None:
+            node = self.root
+        for i, item in enumerate(node.keys):
+            if key < item:
+                if node.is_leaf:
+                    return False
+                else:
+                    return self.search(key, node.children[i])
+            elif key == item:
+                return True
+        if node.is_leaf:
+            return False
+        else:
+            return self.search(key, node.children[-1])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def insert(self, key):
+        """Insert a key into the tree"""
+        root = self.root
+        if len(root.keys) == (2 * self.t) - 1:
+            new_root = Node(is_leaf=False)
+            new_root.children.append(self.root)
+            self.split_child(new_root, 0)
+            self.root = new_root
+        self._insert_non_full(self.root, key)
 
+    def _insert_non_full(self, node, key):
+        """Insert into a node that is not full"""
+        if node.is_leaf:
+            node.keys.append(key)
+            node.keys.sort()
+        else:
+            i = len(node.keys) - 1
+            while i >= 0 and key < node.keys[i]:
+                i -= 1
+            i += 1
+            if len(node.children[i].keys) == (2 * self.t) - 1:
+                self.split_child(node, i)
+                if key > node.keys[i]:
+                    i += 1
+            self._insert_non_full(node.children[i], key)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def split_child(self, parent, index):
+        """Split a full child node"""
+        t = self.t
+        node = parent.children[index]
+        mid = t - 1
+        new_node = Node(is_leaf=node.is_leaf)
+        parent.keys.insert(index, node.keys[mid])
+        parent.children.insert(index + 1, new_node)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Move keys and children to the new node
+        new_node.keys = node.keys[mid + 1:]
+        node.keys = node.keys[:mid]
+
+        if not node.is_leaf:
+            new_node.children = node.children[mid + 1:]
+            node.children = node.children[:mid + 1]
+
